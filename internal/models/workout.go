738982fa05a3c167e9274fa7850062195f6a5242
nReal313 <mohitguleriaiit@gmail.com>
2025-06-24 08:37:58 +0530

    refactored to actual required schema

--- internal/models/workout.go
+++ internal/models/workout.go
@@ -2,18 +2,26 @@ package models
 
 import "time"
 
-type WorkoutLog struct {
+type Workout struct {
+	ID        int           `json:"id" db:"id"`
+	Name      string        `json:"name" db:"name"`
+	Exercises []ExerciseLog `json:"exercises" db:"exercises"`
+	CreatedAt time.Time     `json:"created_at" db:"created_at"`
+}
+
+type ExerciseLog struct {
 	ID        int       `json:"id" db:"id"`
 	Exercise  string    `json:"exercise"`
 	Sets      []Set     `json:"sets"`
+	WorkoutID int       `json:"workout_id" db:"workout_id"`
 	CreatedAt time.Time `json:"created_at" db:"created_at"`
 }
 
 type Set struct {
-	ID        int     `json:"id" db:"id"`
-	WorkoutID int     `json:"workout_id" db:"workout_id"`
-	Reps      int     `json:"reps"`
-	Weight    float64 `json:"weight"`
-	RPE       float64 `json:"rpe"`
-	SetNumber int     `json:"set_number" db:"set_number"`
+	ID         int     `json:"id" db:"id"`
+	ExerciseID int     `json:"exercise_id" db:"exercise_id"`
+	Reps       int     `json:"reps"`
+	Weight     float64 `json:"weight"`
+	RPE        float64 `json:"rpe"`
+	SetNumber  int     `json:"set_number" db:"set_number"`
 }
--- internal/services/database.go
+++ internal/services/database.go
@@ -24,12 +24,26 @@ func NewDatabaseService() (*DatabaseService, error) {
 }
 
 func createTables(db *sql.DB) error {
-	//create workout table
-	_, err := db.Exec(`
-	CREATE TABLE IF NOT EXISTS workouts (
+	//create workoutlog table
+	_, err := db.Exec(
+		`CREATE TABLE IF NOT EXISTS workouts (
+			id INTEGER PRIMARY KEY AUTOINCREMENT,
+			workout_name TEXT NOT NULL, 
+			created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+		)
+	`)
+	if err != nil {
+		return err
+	}
+
+	//create  table
+	_, err = db.Exec(`
+	CREATE TABLE IF NOT EXISTS exercises (
 		id INTEGER PRIMARY KEY AUTOINCREMENT, 
 		exercise TEXT NOT NULL,
+		workout_id INTEGER NOT NULL,
 		created_at DATETIME DEFAULT CURRENT_TIMESTAMP
+		FOREIGN KEY (workout_id) REFERENCES workouts (id)
 		)
 	`)
 	if err != nil {
@@ -39,19 +53,19 @@ func createTables(db *sql.DB) error {
 	_, err = db.Exec(`
 	CREATE TABLE IF NOT EXISTS sets (
 		id INTEGER PRIMARY KEY AUTOINCREMENT,
-		workout_id INTEGER NOT NULL,
+		exercise_id INTEGER NOT NULL,
 		reps INTEGER NOT NULL,
 		weight REAL NOT NULL,
 		rpe REAL NOT NULL,
 		set_number INTEGER NOT NULL,
-		FOREIGN KEY (workout_id) REFERENCES workouts (id)
+		FOREIGN KEY (exercise_id) REFERENCES exercises (id)
 	)
 	`)
 
 	return err
 }
 
-func (s *DatabaseService) SaveWorkout(workout *models.WorkoutLog) error {
+func (s *DatabaseService) SaveWorkout(workout *models.Workout) error {
 	//Start a transaction
 	tx, err := s.db.Begin()
 	if err != nil {
@@ -59,58 +73,94 @@ func (s *DatabaseService) SaveWorkout(workout *models.WorkoutLog) error {
 	}
 	defer tx.Rollback()
 
-	//Insert workout
-	result, err := tx.Exec("INSERT INTO workouts (exercise) VALUES (?)", workout.Exercise)
+	result, err := tx.Exec("INSERT INTO workouts (workout_name) VALUES (?)", workout.Name)
 	if err != nil {
 		return err
 	}
-
 	workoutID, err := result.LastInsertId()
 	if err != nil {
 		return err
 	}
 
-	//Insert sets
-	for i, set := range workout.Sets {
-		_, err := tx.Exec(
-			"INSERT INTO sets (workout_id, reps, weight, rpe, set_number) VALUES (?, ?, ?, ?, ?)",
-			workoutID, set.Reps, set.Weight, set.RPE, i+1,
-		)
+	for _, exercise := range workout.Exercises {
+		//Insert exercises
+		result, err = tx.Exec("INSERT INTO exercises (exercise, workout_id) VALUES (?, ?)", exercise.Exercise, workoutID)
 		if err != nil {
 			return err
+		}
+
+		exerciseID, err := result.LastInsertId()
+		if err != nil {
+			return err
+		}
+
+		//Insert sets
+		for i, set := range exercise.Sets {
+			_, err := tx.Exec(
+				"INSERT INTO sets (exercise_id, reps, weight, rpe, set_number) VALUES (?, ?, ?, ?, ?)",
+				exerciseID, set.Reps, set.Weight, set.RPE, i+1,
+			)
+			if err != nil {
+				return err
+			}
 		}
 	}
 
 	return tx.Commit()
 }
 
-func (s *DatabaseService) GetWorkouts() ([]models.WorkoutLog, error) {
-	var totalLogs []models.WorkoutLog
-	rows, err := s.db.Query("SELECT id, exercise, created_at FROM workouts")
+func (s *DatabaseService) GetWorkouts() ([]models.Workout, error) {
+	var totalLogs []models.Workout
+
+	rows, err := s.db.Query("SELECT id, workout_name, created_at FROM workouts")
 	if err != nil {
 		return nil, err
 	}
 	defer rows.Close()
+
 	for rows.Next() {
-		var log models.WorkoutLog
-		err := rows.Scan(&log.ID, &log.Exercise, &log.CreatedAt)
+		var workout models.Workout
+		err := rows.Scan(&workout.ID, &workout.Name, &workout.CreatedAt)
 		if err != nil {
 			return nil, err
 		}
-		setRows, err := s.db.Query("SELECT reps, weight, rpe, set_number FROM sets WHERE workout_id = ?", log.ID)
+		exerciseRows, err := s.db.Query("SELECT id, exercise FROM exercises WHERE workout_id = ?", workout.ID)
 		if err != nil {
 			return nil, err
 		}
-		defer setRows.Close()
-		for setRows.Next() {
-			var set models.Set
-			err := setRows.Scan(&set.Reps, &set.Weight, &set.RPE, &set.SetNumber)
+		for exerciseRows.Next() {
+			var exerciseLog models.ExerciseLog
+
+			err := exerciseRows.Scan(&exerciseLog.ID, &exerciseLog.Exercise)
 			if err != nil {
 				return nil, err
 			}
-			log.Sets = append(log.Sets, set)
+			setRows, err := s.db.Query("SELECT reps, weight, rpe, set_number FROM sets WHERE exercise_id = ?", exerciseLog.ID)
+			if err != nil {
+				return nil, err
+			}
+			for setRows.Next() {
+				var set models.Set
+				err := setRows.Scan(&set.Reps, &set.Weight, &set.RPE, &set.SetNumber)
+				if err != nil {
+					return nil, err
+				}
+
+				exerciseLog.Sets = append(exerciseLog.Sets, set)
+			}
+			setRows.Close()
+
+			workout.Exercises = append(workout.Exercises, exerciseLog)
 		}
-		totalLogs = append(totalLogs, log)
+		exerciseRows.Close()
+		totalLogs = append(totalLogs, workout)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
 	}
 	return totalLogs, nil
 }
+
+// func (s *DatabaseService) GetWorkout(date time.Time) (models.WorkoutLog, error) {
+// 	rows, err := s.db.Query("SELECT id, exercise, created_at FROM ")
+// }
